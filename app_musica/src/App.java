2a87fea4e0d7c2d0d2173826d6d7aeb606fd1782
thayna <thayna.freire@estudante.ifto.edu.br>
2022-12-01 14:26:24 -0300

    primeiro commit de um membro

--- app_musica/src/App.java
+++ app_musica/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Primeira alteracao de um membro da equipe");
     }
 }
