0b4c498944bb6241d80c498b521cf993d7637a26
thayna <thayna.freire@estudante.ifto.edu.br>
2022-12-07 14:14:06 -0300

    teste

--- app_musica/src/Categoria.java
+++ app_musica/src/Categoria.java
@@ -1,5 +1,35 @@
 public class Categoria {
     private String pais;
+    private String lingua;
+    private date data_lac;
 
-    
+    public categoria(){
+
+    public String getPais() {
+        return this.pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getLingua() {
+        return this.lingua;
+    }
+
+    public void setLingua(String lingua) {
+        this.lingua = lingua;
+    }
+
+    public date getData_lac() {
+        return this.data_lac;
+    }
+
+    public void setData_lac(date data_lac) {
+        this.data_lac = data_lac;
+    }
+
+    }
+
+
 }
