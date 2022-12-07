3ffcfba53dbfb7e44e442e1f6fcb71eb3824b75f
unknown <Aluno@CLN-041434.ifto.local>
2022-12-07 15:30:46 -0300

    Criacao da classe Genero

---
+++ app_musica/src/Genero.java
@@ -0,0 +1,12 @@
+Public class Genero{
+    String nome;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
